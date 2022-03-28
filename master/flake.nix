7279b15cedf7fd3543f2bcdff49abaa000541e0b
hugosenari <hugosenari@gmail.com>
2022-03-27 21:57:20 -0300

    chore: re index treesitter_agda

--- master/flake.nix
+++ master/flake.nix
@@ -11,8 +11,8 @@
   inputs.src-treesitter_agda-master.ref   = "refs/heads/master";
   inputs.src-treesitter_agda-master.owner = "genotrance";
   inputs.src-treesitter_agda-master.repo  = "nimtreesitter";
-  inputs.src-treesitter_agda-master.dir   = "treesitter_agda";
-  inputs.src-treesitter_agda-master.type  = "github";
+    inputs.src-treesitter_agda-master.dir   = "treesitter_agda";
+inputs.src-treesitter_agda-master.type  = "github";
   
   inputs."nimgen".owner = "nim-nix-pkgs";
   inputs."nimgen".ref   = "master";
